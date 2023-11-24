91143e91328043d8c491e3314ff5aff8f3a4272a
JennyXim <150806167+JennyXim@users.noreply.github.com>
2023-11-24 12:49:17 -0500

    CAMBIO3

--- main.java
+++ main.java
@@ -11,27 +11,31 @@ public class Main {
         Notas minota4;
         Notas minota5;
 
-        minota1=new Notas("Jennyfer");
+        System.out.println("Escribe tu nombre");
+        minota1=new Notas(nombre=variable.nextLine());
         minota2=new Notas("Samanta");
         minota3=new Notas("Ximena");
         minota4=new Notas("Kelyn");
         minota5=new Notas("Samira");
         //  nombre=variable.nextLine();
 
-        minota1.setApellido("Guayanay");
+        System.out.println("Escribe tu Apellido");
+        minota1.setApellido(nombre=variable.nextLine());
         minota2.setApellido("Torres");
         minota3.setApellido("Guayanay");
         minota4.setApellido("Guayanay");
         minota5.setApellido("Guayanay");
 
 
-        minota1.setNota1Bimestre("8");
+        System.out.println("Escribe tu Nota del primer Bimestre");
+        minota1.setNota1Bimestre(nombre=variable.nextLine());
         minota2.setNota1Bimestre("8");
         minota3.setNota1Bimestre("8");
         minota4.setNota1Bimestre("8");
         minota5.setNota1Bimestre("8");
 
-        minota1.setNota2Bimestre("5");
+        System.out.println("Escribe tu Nota del segundo Bimestre");
+        minota1.setNota2Bimestre(nombre=variable.nextLine());
         minota2.setNota2Bimestre("5");
         minota3.setNota2Bimestre("5");
         minota4.setNota2Bimestre("5");
@@ -41,7 +45,7 @@ public class Main {
         //obtener los valores de cada atributos
 
         System.out.println("SISTEMAS DE NOTAS ....................");
-        System.out.println("Nombre: "+minota1.getNombre()+" Apellido: "+minota1.getApellido()+" Nota 1 Bimestre: "+minota1.getNota1Bimestre()+" Nota 2 Bimestre: "+minota2.getNota2Bimestre()+"\n");
+        System.out.println("Nombre: "+minota1.getNombre()+"\n"+"Apellido: "+minota1.getApellido()+"\n"+"Nota 1 Bimestre: "+minota1.getNota1Bimestre()+"\n"+"Nota 2 Bimestre: "+minota1.getNota2Bimestre()+"\n");
 
     }
 }
